9b808561347f8b44cea0c05d706809b57e084cba
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-10-22 13:11:03 -0700

    created simple post request example

--- src/routes.rs
+++ src/routes.rs
@@ -4,6 +4,9 @@ use rocket::{
     self,
     Config,
     fs::NamedFile,
+    serde::json::Json,
+    serde::Deserialize,
+    serde::Serialize,
 };
 
 use rocket_dyn_templates::Template;
@@ -20,6 +23,18 @@ async fn get_file(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("public/").join(file)).await.ok()
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct MyData {
+    Id: i32,
+}
+
+#[rocket::post("/report", data = "<data>")]
+fn report(data: Json<MyData>) -> String {
+    println!("{:?}", data.Id);
+    data.Id.to_string()
+}
+
 pub fn start_api() {
     rocket::tokio::runtime::Builder::new_multi_thread()
         .worker_threads(Config::from(Config::figment()).workers)
@@ -30,7 +45,7 @@ pub fn start_api() {
         .expect("create tokio runtime")
         .block_on(async move {
             let _ = rocket::build()
-            .mount("/", rocket::routes![index, get_file])
+            .mount("/", rocket::routes![index, get_file, report])
             .attach(Template::fairing())
             //.manage()
             .launch()
