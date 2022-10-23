98082ede616880698b29508b210e11240b5b6753
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-10-22 18:18:42 -0700

    made it so only one marker can be placed at a time changed map to dark mode

--- public/map.js
+++ public/map.js
@@ -1,10 +1,16 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2FybXN0IiwiYSI6ImNsOWJydXhzNjNyN2EzdmxmZW00bDN0OTEifQ.4pmON-6Tdj9ca_T8_8D_dw';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/dark-v10'
 });
 
 map.on('click', (e) => {
+    let old_marker = document.getElementById("user marker");
+
+    if (old_marker != null) {
+        old_marker.remove();
+    }
+
     var coords = `lat: ${e.lngLat.lat} <br> lng: ${e.lngLat.lng}`;
     
     console.log(e.lngLat.lat);
@@ -15,7 +21,7 @@ map.on('click', (e) => {
 
     // create DOM element for the marker
     var el = document.createElement('div');
-    el.id = 'marker';
+    el.id = 'user marker';
 
     // create the marker
     new mapboxgl.Marker(el)
