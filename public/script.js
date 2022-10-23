dd4a1f671c10f94723bcda0fa3f57e02d614482b
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-10-22 18:09:50 -0700

    added user registration added sending users crime alerts within range

--- public/script.js
+++ public/script.js
@@ -16,3 +16,21 @@ let post_crime = function(lat, long, desc) {
 
     xhr.send(JSON.stringify(data));
 }
+
+let register_user = function(num, lat, long) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/register");
+
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = () => console.log(xhr.responseText);
+
+    let data = {
+        "num": num,
+        "lat": lat,
+        "long": long,
+    };
+
+    xhr.send(JSON.stringify(data));
+}
--- src/routes.rs
+++ src/routes.rs
@@ -4,6 +4,8 @@ use std::io::prelude::*;
 use std::vec;
 use regex::Regex;
 
+use twilio_async::{Twilio, TwilioRequest};
+
 use rocket::{
     self,
     Config,
@@ -20,13 +22,8 @@ use rocket_dyn_templates::Template;
 
 #[rocket::get("/")]
 async fn index() -> Template {
-    println!("{:?}", get_crimes());
-
-    for crime in get_crimes().unwrap() {
-       println!("{}", get_address(crime.lat, crime.long).await);
-    }
-
-    Template::render("index", rocket_dyn_templates::context!{crimes: get_crimes().unwrap()})
+    println!("{:?}", get_users().unwrap());
+    Template::render("index", rocket_dyn_templates::context!{crimes: get_crimes().await.unwrap()})
 }
 
 
@@ -44,15 +41,28 @@ struct Crime {
     loc: String,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct User {
+    lat: f32,
+    long: f32,
+    num: i64,
+}
+
 #[rocket::post("/report", data = "<data>")]
-fn report(data: Json<Crime>) -> String {
-    let crime: Crime = Crime{lat: data.lat, long: data.long, desc: data.desc.clone(), loc: "".to_string()};
-    add_crime(crime);
+async fn report(data: Json<Crime>) -> String {
+    let mut crime: Crime = Crime{lat: data.lat, long: data.long, desc: data.desc.clone(), loc: "".to_string()};
+    add_crime(&crime);
     println!("{:?}", data);
+
+    crime.loc = get_address(crime.lat, crime.long).await;
+
+    handle_notifications(crime).await;
+
     "200".to_string()
 }
 
-fn add_crime(crime: Crime) {
+fn add_crime(crime: &Crime) {
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
@@ -62,6 +72,82 @@ fn add_crime(crime: Crime) {
 
     file.write_all(format!("\n{}, {}, {}", crime.lat, crime.long, crime.desc.clone()).as_bytes()).expect("error writing crime");
 }
+
+
+async fn get_crimes() -> Result<Vec<Crime>, String> {
+    let contents = match read_to_string("crimes.txt") {
+        Ok(n) => {n},
+        Err(_) => {return Err("error reading file".to_string());},
+    };
+    
+    let mut crimes: Vec<Crime> = vec![];
+
+    for line in contents.split("\n") {
+        let vals: Vec<&str> = line.split(",").collect();
+
+        if vals.len() != 3 {
+            continue;
+        }
+
+        let lat: f32 = vals[0].trim().parse().unwrap();
+        let long: f32 = vals[1].trim().parse().unwrap();
+        let desc: String = vals[2].to_string();
+        let loc: String = get_address(lat, long).await;
+
+        let crime = Crime{lat, long, desc, loc};
+        crimes.push(crime);
+    }
+
+    Ok(crimes)
+}
+
+#[rocket::post("/register", data = "<data>")]
+fn register(data: Json<User>) -> String {
+    let user: User = User{num: data.num, lat: data.lat, long: data.long};
+    add_user(user);
+    println!("{:?}", data);
+    "200".to_string()
+}
+
+
+
+fn add_user(user: User) {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .append(true)
+        .open("users.txt")
+        .expect("could not open file");
+
+    file.write_all(format!("\n{}, {}, {}", user.num, user.lat, user.long).as_bytes()).expect("error writing user");
+}
+
+fn get_users() -> Result<Vec<User>, String> {
+    let contents = match read_to_string("users.txt") {
+        Ok(n) => {n},
+        Err(_) => {return Err("error reading file".to_string());},
+    };
+    
+    let mut users: Vec<User> = vec![];
+
+    for line in contents.split("\n") {
+        let vals: Vec<&str> = line.split(",").collect();
+
+        if vals.len() != 3 {
+            continue;
+        }
+
+        let num: i64 = vals[0].trim().parse().unwrap();
+        let lat: f32 = vals[1].trim().parse().unwrap();
+        let long: f32 = vals[2].trim().parse().unwrap();
+
+        let user = User{num, lat, long};
+        users.push(user);
+    }
+
+    Ok(users)
+}
+
 
 async fn get_address(lat: f32, long: f32) -> String {
     let token: &str = "pk.eyJ1Ijoid2FybXN0IiwiYSI6ImNsOWJydXhzNjNyN2EzdmxmZW00bDN0OTEifQ.4pmON-6Tdj9ca_T8_8D_dw";
@@ -85,27 +171,33 @@ async fn get_address(lat: f32, long: f32) -> String {
     format!("lat:{}, long:{}", lat, long).to_string()
 }
 
-
-fn get_crimes() -> Result<Vec<Crime>, String> {
-    let contents = match read_to_string("crimes.txt") {
-        Ok(n) => {n},
-        Err(_) => {return Err("error reading file".to_string());},
-    };
-    
-    let mut crimes: Vec<Crime> = vec![];
-
-    for line in contents.split("\n") {
-        let vals: Vec<&str> = line.split(",").collect();
-
-        if vals.len() != 3 {
-            continue;
+async fn handle_notifications(crime: Crime) {
+    for user in get_users().unwrap() {
+        if get_distance(&user, &crime) <= 6f32 {
+            println!("sending to user {}", user.num);
+            send_alert(&user, &crime).await;
         }
-
-        let crime = Crime{lat: vals[0].trim().parse().unwrap(), long: vals[1].trim().parse().unwrap(), desc: vals[2].to_string(), loc: "".to_string()};
-        crimes.push(crime);
     }
+}
 
-    Ok(crimes)
+fn get_distance(user: &User, crime: &Crime) -> f32{
+    0f32
+}
+
+
+async fn send_alert(user: &User, crime: &Crime) {
+    let twilio = Twilio::new("AC9500dd8203f63e10eb400155ed142e1d", "ffc4e47ab697e99d441b9e659c42a87a").unwrap();
+    
+    let mut msg = String::new();
+    msg += "Alert! A crime has been reported within 6 miles of you're location\n";
+    msg += "Location | ";
+    msg += crime.loc.as_str();
+    msg += "\nDescription | ";
+    msg += crime.desc.as_str();
+
+    println!("{}", ("+".to_string() + user.num.to_string().as_str()).as_str());
+
+    twilio.send_msg("+19704272763", ("+".to_string() + user.num.to_string().as_str()).as_str(), msg.as_str()).run().await.unwrap();
 }
 
 pub fn start_api() {
@@ -118,7 +210,7 @@ pub fn start_api() {
         .expect("create tokio runtime")
         .block_on(async move {
             let _ = rocket::build()
-            .mount("/", rocket::routes![index, get_file, report])
+            .mount("/", rocket::routes![index, get_file, report, register])
             .attach(Template::fairing())
             //.manage()
             .launch()
